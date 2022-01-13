9baa4907abb6b155f0905d07938b04a9b4edb55a
SrdjanLazic <slazic3219rn@raf.rs>
2022-01-13 15:40:02 +0100

    Initial commit

---
+++ hotel-eureka-service/src/main/java/com/raf/hoteleurekaservice/HotelEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raf.hoteleurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelEurekaServiceApplication.class, args);
+	}
+
+}
